8c3a8b1682a3a9c37d0357dfbcb26290f669a0c3
bdimpe@gmail <bdimpe@gmail>
2015-09-17 18:13:15 -0400

    added f# version of build definition backup that includes tfAdd and tfCheckout for the build definition backup it serializes

--- LINQPad Queries/Tfs/ViaTfsDlls/SaveBuildDefinition.fsx
+++ LINQPad Queries/Tfs/ViaTfsDlls/SaveBuildDefinition.fsx
@@ -32,19 +32,24 @@ let runProc filename args startDir =
     p.WaitForExit()
     outputs,errors
 
-module TfExe = 
-    let tf startDir args = 
-        runProc @"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\TF.exe" args startDir
-
-    let tfAdd basePath items =
-        let addItem item = sprintf "add %s" item |> tf basePath
-        let readOutputs (o,e) = 
+let readOutputs (o,e) = 
             if e|> Seq.exists (fun e' -> String.IsNullOrWhiteSpace( e' ) = false) then
                 o |> Seq.iter (printfn "%s")
                 e |> Seq.iter (printfn "error:%s")
             else
                 o |> Seq.iter (printfn "%s")
+module TfExe = 
+    let tfPath = @"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\TF.exe"
+    let tf startDir args = runProc tfPath args startDir
+
+    // TfExe.tfAdd None [@"C:\tfs\XpressCharts2\BuildDefinitionBackups\FullBuild.json"];;
+    let tfAdd basePath items =
+        let addItem item = sprintf "add %s" item |> tf basePath
         items |> Seq.iter (addItem >> readOutputs)
+    let tfCheckout basePath items = 
+        let coItem item = sprintf "checkout %s" item |> tf basePath
+        items |> Seq.iter (coItem >> readOutputs)
+            
 
 #if UseFsi
 let tfs = TFS.GetTfs();
@@ -72,116 +77,187 @@ let teamProjectCollectionUri = teamProjectCollection.Uri
 
 let getBuildDefinitions() = build.GetBuildDefinitions(teamProjectInfo.Name)
 
-module LiteralSerializer = // holds the direct translated C# code and types 
-    type BuildDefinition = {
-        Uri: Uri
-        Id:string
-        TeamProject:string
-        CollectionUri:Uri
-        Name:string
-        Description:string
-        QueueStatus: DefinitionQueueStatus
-        Trigger:Trigger
-        Schedules: Schedule[]
-        Workspace: Workspace
-        BuildController:BuildController
-        DefaultDropLocation:string
-        Process:ProcessTemplate
-        ProcessParameters:string
-        RetentionPolicies:RetentionPolicy[]
-        } and
-        Trigger = {
-            TriggerType: DefinitionTriggerType
-            ContinuousIntegrationType: ContinuousIntegrationType
-            ContinuousIntegrationQuietPeriodMinutes: int
-            BatchSize:int
-        } and
-        Schedule = {
-            DaysToBuild:ScheduleDays
-            StartTimeSecondsPastMidnight: int
-            TimeZone: TimeZoneInfo
-            Type: ScheduleType
-        } and
-        Workspace = { Mappings:Mapping[] } 
+module ExpressionHelpers = 
+    open System.Linq.Expressions
+    open System.Reflection
+    open Microsoft.FSharp.Quotations.Patterns
+    open Microsoft.FSharp.Quotations.DerivedPatterns
+    open Microsoft.FSharp.Quotations.ExprShape
+//
+//    //method taken from http://stackoverflow.com/q/4604139/57883
+//    let methodSourceName (mi:MemberInfo) =
+//        mi.GetCustomAttributes(true)
+//        |> Array.tryPick 
+//                (function
+//                    | :? CompilationSourceNameAttribute as csna -> Some(csna)
+//                    | _ -> None)
+//        |> (function | Some(csna) -> csna.SourceName | None -> mi.Name)
+    let rec getName expr = 
+        match expr with 
+        |ShapeVar v -> v.Name
+        |ShapeLambda (v,expr) -> v.Name
+        |PropertyGet (_,p,_) -> p.Name
+        |ShapeCombination (o, exprs) -> failwithf "not sure what to do with %A %A" o exprs
+
+    let rec eval expr = // from http://stackoverflow.com/a/6404395/57883
+        match expr with
+        | Value(value,_) -> value //value
+        | PropertyGet(Some(instance), pi, args) -> //instance property get
+            pi.GetValue(eval instance, evalAll args) //notice recursive eval of instance expression and arg expressions
+        | PropertyGet(None, pi, args) -> //static property get
+            pi.GetValue(null, evalAll args)
+        | Call(Some(instance), mi, args) -> //instance call
+            mi.Invoke(eval instance, evalAll args)
+        | Call(None, mi, args) -> //static call
+            mi.Invoke(null, evalAll args)
+        | _ -> failwith "invalid expression"
+    and evalAll exprs = exprs |> Seq.map eval |> Seq.toArray
+
+module JsonSerializationHelpers =
+    open Newtonsoft.Json
+    let private nameof expr = ExpressionHelpers.getName expr //|> (fun c -> c.Name)
+    let private eval expr = ExpressionHelpers.eval expr
+    type JsonObject = {Name:string; Properties:JsonProp seq} 
         and
-        Mapping = {
-            MappingType:WorkspaceMappingType
-            ServerItem:string
-            LocalItem:string
-            Depth:WorkspaceMappingDepth
-        } and
-        BuildController = {
-            BuildControllerUri:Uri
-            Name:string
-            Description:string
-            CustomAssemblyPath:string
-        } and
-        ProcessTemplate = {
-            Id:int
-            Description:string
-            ServerPath:string
-            SupportedReasons:BuildReason
-            TeamProject:string
-            TemplateType:ProcessTemplateType
-            Version:Version
-        } and
-        RetentionPolicy = {
-            BuildReason:BuildReason
-            BuildStatus:BuildStatus
-            DeleteOptions: DeleteOptions
-            NumberToKeep:int
-        }
+        JsonProp = 
+        |Expr of Quotations.Expr
+        |ExprComplex of Quotations.Expr
+        |NameValue of string*obj
+        |PropObject of JsonObject
+        |PropObjArray of string * JsonProp list seq
+    let rec writeProperty (w:JsonTextWriter) prop =
+        let inline writeProperty x = writeProperty w x 
+        match prop with
+        |Expr expr -> 
+            w.WritePropertyName(nameof expr)
+            w.WriteValue (eval expr)
+        |ExprComplex expr ->
+            w.WritePropertyName (nameof expr)
+            let value = ExpressionHelpers.eval expr
+            let value' = JsonConvert.SerializeObject(value)
+            let deserialized = JsonConvert.DeserializeObject(value')
+            w.WriteRawValue value'
+        |NameValue (n,v) ->
+            w.WritePropertyName n
+            w.WriteValue v
+        |PropObject jo ->
+            w.WritePropertyName jo.Name
+            w.WriteStartObject()
+            jo.Properties |> Seq.iter writeProperty
+            w.WriteEndObject()
+        |PropObjArray (name,props) ->
+            w.WritePropertyName name
+            w.WriteStartArray()
+            props |> Seq.iter (fun oProps -> 
+                w.WriteStartObject()
+                oProps |> Seq.iter writeProperty
+                w.WriteEndObject()
+                )
+            w.WriteEndArray()
 
+open JsonSerializationHelpers
+let serializeBuild (teamProjectCollectionUri:string) (build:IBuildDefinition) = 
+    use sw = new IO.StringWriter()
+    use w = new Newtonsoft.Json.JsonTextWriter(sw)
+    w.WriteStartObject()
+    let nameof expr = ExpressionHelpers.getName expr
 
-    let mapBuild (buildToDump:IBuildDefinition)= 
-        {
-            Uri=buildToDump.Uri
-            Id= buildToDump.Id
-            TeamProject = buildToDump.TeamProject
-            CollectionUri = teamProjectCollectionUri
-            Name = buildToDump.Name
-            Description = buildToDump.Description
-            QueueStatus = buildToDump.QueueStatus
-            Trigger = 
-                {
-                    TriggerType = buildToDump.TriggerType
-                    ContinuousIntegrationType = buildToDump.ContinuousIntegrationType
-                    ContinuousIntegrationQuietPeriodMinutes = buildToDump.ContinuousIntegrationQuietPeriod
-                    BatchSize = buildToDump.BatchSize
-                }
-            Schedules = 
-                buildToDump.Schedules 
-                |> Seq.map (fun s -> { DaysToBuild = s.DaysToBuild; StartTimeSecondsPastMidnight = s.StartTime; TimeZone = s.TimeZone; Type = s.Type })
-                |> Array.ofSeq
-            Workspace = { Mappings= buildToDump.Workspace.Mappings |> Seq.map (fun m -> {MappingType = m.MappingType; ServerItem = m.ServerItem; LocalItem = m.LocalItem; Depth = m.Depth}) |> Array.ofSeq}
-            BuildController = {BuildControllerUri = buildToDump.BuildControllerUri; Name = buildToDump.BuildController.Name; Description = buildToDump.BuildController.Description; CustomAssemblyPath = buildToDump.BuildController.CustomAssemblyPath}
-            DefaultDropLocation = buildToDump.DefaultDropLocation
-            Process= 
-                {
-                    Id = buildToDump.Process.Id
-                    Description = buildToDump.Process.Description
-                    ServerPath = buildToDump.Process.ServerPath
-                    SupportedReasons = buildToDump.Process.SupportedReasons
-                    TeamProject = buildToDump.Process.TeamProject
-                    TemplateType = buildToDump.Process.TemplateType
-                    Version = buildToDump.Process.Version
-                }
-            ProcessParameters= buildToDump.ProcessParameters
-            RetentionPolicies = buildToDump.RetentionPolicyList |> Seq.map (fun rpl -> { BuildReason = rpl.BuildReason; BuildStatus = rpl.BuildStatus; DeleteOptions = rpl.DeleteOptions; NumberToKeep = rpl.NumberToKeep}) |> Array.ofSeq
-        }
+    let writeProperty prop = JsonSerializationHelpers.writeProperty w prop
 
-    let buildToDump = build.GetBuildDefinition(teamProjectInfo.Name, "FullBuild")
-    let saveBuildDefinitions savePath builds = 
-        let paths = 
-            builds
-            |> Seq.map mapBuild
-            |> Seq.map (fun b ->
-                let json = Newtonsoft.Json.JsonConvert.SerializeObject(b,Newtonsoft.Json.Formatting.Indented)
-                let targetPath = IO.Path.Combine(savePath,b.Name + ".json")
-                if System.IO.Directory.Exists savePath=false then
-                    IO.Directory.CreateDirectory(savePath) |> ignore
-                IO.File.WriteAllText(targetPath,json)
-                targetPath
+    let trigger = 
+        PropObject { 
+            Name="Trigger"
+            Properties = [
+                            Expr <@ build.TriggerType @>
+                            Expr <@ build.ContinuousIntegrationType @>
+                            NameValue ("ContinuousIntegrationQuietPeriodMinutes",build.ContinuousIntegrationQuietPeriod)
+                            Expr <@ build.BatchSize @>
+        ]}
+    let schedules = 
+        build.Schedules 
+        |> Seq.map (fun s ->
+                    [
+                        Expr <@ s.DaysToBuild @>
+                        NameValue ("StartTimeSecondsPastMidnight", s.StartTime)
+                        Expr <@ s.TimeZone @>
+                    ]
             )
-        paths |> printfn "Saved to %A"
+    let schedules = PropObjArray ("Schedules", schedules)
+    let mappings =   
+        build.Workspace.Mappings 
+        |> Seq.map (fun m -> 
+            [
+                Expr <@ m.MappingType @>
+                Expr <@ m.ServerItem @>
+                Expr <@ m.LocalItem @>
+                Expr <@ m.Depth @>
+            ])
+    let retentionPolicies = 
+        build.RetentionPolicyList 
+        |> Seq.map (fun rpl -> 
+            [
+                Expr <@ rpl.BuildReason @>
+                Expr <@ rpl.BuildStatus @>
+                Expr <@ rpl.DeleteOptions @>
+                Expr <@ rpl.NumberToKeep @>
+            ])
+
+    [
+        Expr <@ build.Uri @> // build.Uri
+        Expr <@ build.Id @> //build.Id
+        Expr <@ build.TeamProject @> //build.TeamProject
+        NameValue ("CollectionUri",teamProjectCollectionUri)
+        Expr <@ build.Name @> //build.Name
+        Expr <@ build.Description @> //build.Description
+        Expr <@ build.QueueStatus @>
+        trigger
+        schedules
+        PropObject {Name="Workspace";Properties = [PropObjArray ("Mappings", mappings)]}
+        PropObject {Name="BuildController";Properties=
+            [
+                Expr <@ build.BuildControllerUri @>
+                Expr <@ build.BuildController.Name @>
+                Expr <@ build.BuildController.Description @>
+                Expr <@ build.BuildController.CustomAssemblyPath @>
+            ]}
+        Expr <@ build.DefaultDropLocation @>
+        PropObject {Name="Process"; Properties = [
+                                                Expr <@ build.Process.Id @>
+                                                Expr <@ build.Process.Description @>
+                                                Expr <@ build.Process.ServerPath @>
+                                                Expr <@ build.Process.SupportedReasons @>
+                                                Expr <@ build.Process.TeamProject @>
+                                                Expr <@ build.Process.TemplateType @>
+                                                ExprComplex <@ build.Process.Version @>
+        ]}
+        Expr <@ build.ProcessParameters@>
+        PropObjArray ("RetentionPolicies", retentionPolicies)
+    ] |> Seq.iter writeProperty
+    w.WriteEndObject()
+    build.Name,sw.ToString()
+
+let buildToDump = build.GetBuildDefinition(teamProjectInfo.Name, "FullBuild")
+
+let doCheckout targetPath = 
+    if IO.File.Exists(targetPath) && IO.FileInfo(targetPath).Attributes.HasFlag IO.FileAttributes.ReadOnly then
+        printfn "checking out from tfs"
+        TfExe.tfCheckout None [targetPath]
+
+// saveBuildDefinitions teamProjectInfo.BuildDefinitionSavePath [buildToDump];;
+let saveBuildDefinitions savePath builds = 
+    let paths = 
+        builds
+        |> Seq.map (serializeBuild (string teamProjectCollectionUri))
+        |> Seq.map (fun (name,serialized) ->
+            let deserialized = Newtonsoft.Json.JsonConvert.DeserializeObject(serialized)
+            let json = Newtonsoft.Json.JsonConvert.SerializeObject(deserialized,Newtonsoft.Json.Formatting.Indented)
+
+            let targetPath = IO.Path.Combine(savePath,name + ".json")
+            doCheckout targetPath
+            if System.IO.Directory.Exists savePath=false then
+                IO.Directory.CreateDirectory(savePath) |> ignore
+            IO.File.WriteAllText(targetPath,json)
+            targetPath
+        )
+    paths |> printfn "Saved to %A"
 
--- LINQPad Queries/Tfs/ViaTfsDlls/findByExtension.linq
+++ LINQPad Queries/Tfs/ViaTfsDlls/findByExtension.linq
@@ -1,7 +1,7 @@
 <Query Kind="Statements">
   <Reference>&lt;ProgramFilesX86&gt;\Microsoft Visual Studio 12.0\Common7\IDE\ReferenceAssemblies\v2.0\Microsoft.TeamFoundation.Client.dll</Reference>
   <Reference>&lt;ProgramFilesX86&gt;\Microsoft Visual Studio 12.0\Common7\IDE\ReferenceAssemblies\v2.0\Microsoft.TeamFoundation.VersionControl.Client.dll</Reference>
-  <Reference>C:\projects\Fsi\tfsmacros.dll</Reference>
+  <Reference Relative="..\..\..\..\Fsi\tfsmacros.dll">C:\projects\Fsi\tfsmacros.dll</Reference>
   <NuGetReference>Newtonsoft.Json</NuGetReference>
   <Namespace>Microsoft.TeamFoundation.Client</Namespace>
   <Namespace>Microsoft.TeamFoundation.VersionControl.Client</Namespace>
@@ -17,14 +17,11 @@ vcs.WebServiceLevel.Dump("Tfs info?");
 
 //vcs.GetItems("*.user", RecursionType.Full).Dump();
 var bads= new[]{
-	"**.user",
-	"**.suo",
-	"**resharper**",
-	"**.DS_Store", //http://en.wikipedia.org/wiki/.DS_Store
+	"**.bat",
 	// , "**/packages/**" 
 	};
 	
-var targetPath = "$/XpressCharts/Source-Development/";
+var targetPath = "$/";
 //check the directory path for validity, throws if not found
 try
 {	        
@@ -41,7 +38,7 @@ var badItems=new List<Item>();
 foreach(var item in bads){
 	var items = vcs.GetItems(targetPath+item, RecursionType.Full);
 	badItems.AddRange(items.Items);
-	items.Items.Select (i => i.ServerItem).DumpIf(x=>x.Any(),item+ " is bad");	
+	items.Items.Select (i => i.ServerItem).DumpIf(x=>x.Any(),item);	
 }
 
 
@@ -51,12 +48,3 @@ if(myPending==null)
 return;
 
 myPending.Dump("checking workspace for changes",1);
-
-foreach(var i in badItems){ //will not delete items that are not mapped into your current workspace
-	myPending.PendDelete(i.ServerItem.Dump()).DumpIf(a=>a!=0, "Pend delete error code for "+i.ServerItem);
-}
-var myChanges=myPending.GetPendingChanges().Select (p => new{p.ChangeType, p.ServerItem});
-myChanges
-	//.Where (c => c.ChangeType.Has( ChangeType.Delete) )
-	.Dump("pendingchanges",1);
-//http://msdn.microsoft.com/en-us/magazine/jj553516.aspx
